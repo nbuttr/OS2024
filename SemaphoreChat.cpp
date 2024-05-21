01948972a18be734fde39a98c0b78ea9d6368f37
nbuttr <90682586+nbuttr@users.noreply.github.com>
2024-05-21 16:21:43 +0300

    Add files via upload

---
+++ SemaphoreChat.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <thread>
+#include <windows.h>
+
+using namespace std;
+
+bool messageRead = false;
+HANDLE semaphore;
+
+void writeMessageToFile() {
+    std::ofstream file("chat.txt", std::ios::in);
+   
+    while (true) {
+        file.open("chat.txt", ios::app);
+        if (file.is_open()) {
+            std::string message;
+            std::cout << "Enter your message: ";
+            std::getline(std::cin, message);
+            file << message << '\n';
+            messageRead = true;
+            file.close();
+            ReleaseSemaphore(semaphore, 2, NULL);
+        }
+    }
+    
+
+
+}
+
+void readMessageFromFile() {
+    while (true) {
+        WaitForSingleObject(semaphore, INFINITE);
+
+        std::ifstream file("chat.txt");
+        if (file.is_open()) {
+            std::string text;
+            if (!messageRead) {
+                while (std::getline(file, text)) {
+                    std::cout << "user: " + text << std::endl;
+                }
+                file.close();
+            }
+            messageRead = false;
+        }
+    }
+}
+
+    int main() {
+        semaphore = CreateSemaphoreA(NULL, 0, 2, "semaphore_name");
+        std::thread writerThread(writeMessageToFile);
+        std::thread readerThread(readMessageFromFile);
+
+        writerThread.join();
+        readerThread.join();
+        return 0;
+    }
